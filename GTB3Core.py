b147b2bb7f05d541192b4290e05958ce6f8e9cae
etoGrassie <etoGrassie@outlook.com>
2022-05-09 14:07:25 +0800

    050922

--- GTB3Core.py
+++ GTB3Core.py
@@ -15,16 +15,27 @@ class GTB3MainWindow(Ui_GTBMainWindow, QMainWindow, QObject):
 
     def __init__(self):
         super(GTB3MainWindow, self).__init__()
-        self.default_theme = 'light_blue'
+        self.file_path = None
+        self.default_theme = 'dark_blue'
         self.app = QtWidgets.QApplication(sys.argv)
         self.setupUi(self)
-        self.initUi()
+        self.init_link()
 
-    def initUi(self):
-        self.pushButton_open_file.clicked.connect(self.open_file_dialog)
+    def init_link(self):
+        self.pushButton_open_file.clicked.connect(self.open_file)
 
         self.actionDark_Mode.triggered.connect(lambda: self.use_theme(theme='dark_blue'))
         self.actionDefault_Light.triggered.connect(lambda: self.use_theme(theme='light_blue'))
+
+    def open_file(self):
+        result = self.open_file_dialog()
+        if result is not None:
+            with open(self.file_path, 'r') as file:
+                file_index = file.readlines()
+            self.textBrowser_file_preview.setPlainText(''.join(file_index))
+            return
+        else:
+            return
 
     def use_theme(self, theme):
         apply_stylesheet(self.app, theme=theme + '.xml')
@@ -32,10 +43,21 @@ class GTB3MainWindow(Ui_GTBMainWindow, QMainWindow, QObject):
     def open_file_dialog(self):
         dialog = QFileDialog.getOpenFileName(self, 'Open Task Command File', './', 'Task Command File ('
                                                                                    '*.tskx)')
-        self.file_path = dialog[0]
+        if bool(dialog[0]) is True:
+            self.file_path = dialog[0]
+            return True
+        else:
+            self.file_path = None
+            return False
 
     def save_file_dialog(self):
         dialog = QFileDialog.getSaveFileName(self, 'Save Output File', './', 'Task Command File (*.tskx)')
+        if bool(dialog[0]) is True:
+            self.file_path = dialog[0]
+            return True
+        else:
+            self.file_path = None
+            return False
 
 
 if __name__ == "__main__":
