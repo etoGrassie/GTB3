25a01a3b170dda1f8f08cfd277542aa0c4892a0d
etoGrassie <etoGrassie@outlook.com>
2022-05-09 13:20:49 +0800

    050922

--- GTB3Main.py
+++ GTB3Main.py
@@ -1,39 +1,40 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QMainWindow
 from qt_material import apply_stylesheet
-import GTB3Thread
+
 from GTB3Window import Ui_GTBMainWindow
 
 
-class GTB3MainWindow(Ui_GTBMainWindow, QMainWindow):
+class GTB3MainWindow(Ui_GTBMainWindow, QMainWindow, QObject):
     """
     GTB3MainWindow is used to add more functions or features to GTB3Window.py. 
     """
 
     def __init__(self):
+        self.default_theme = 'light_blue'
         self.app = QtWidgets.QApplication(sys.argv)
         super(GTB3MainWindow, self).__init__()
         self.setupUi(self)
-        self.setupFunctions()
+        self.initUi()
 
-    def setupFunctions(self):
+    def initUi(self):
         self.pushButton_open_file.clicked.connect(self.test)
-        
-        self.actionDark_Mode.triggered.connect(lambda: self.use_theme(theme = 'dark_blue'))
-        self.actionDefault_Light.triggered.connect(lambda: self.use_theme(theme = 'light_blue'))
 
-    def test(self):
-        print("hello")
+        self.actionDark_Mode.triggered.connect(lambda: self.use_theme(theme='dark_blue'))
+        self.actionDefault_Light.triggered.connect(lambda: self.use_theme(theme='light_blue'))
+
+    def file_mode_proceed(self):
         self.pushButton_open_file.setText("hi")
-        
+
     def use_theme(self, theme):
-            apply_stylesheet(self.app, theme = theme + '.xml')
-            
+        apply_stylesheet(self.app, theme=theme + '.xml')
+
 
 if __name__ == "__main__":
     ui = GTB3MainWindow()
     app = ui.app
-    # apply_stylesheet(app, theme = 'light_blue.xml')
+    apply_stylesheet(app, theme=ui.default_theme + '.xml')
     ui.show()
     sys.exit(app.exec_())
