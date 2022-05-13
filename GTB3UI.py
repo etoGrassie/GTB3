f9488ff232bc04ada4c7002046b8ed8d81287052
etoGrassie <etoGrassie@outlook.com>
2022-05-13 13:13:33 +0800

    051322

--- GTB3UI.py
+++ GTB3UI.py
@@ -1,39 +1,10 @@
-from PyQt5.QtCore import pyqtSignal
-
-import GTB3Applications
-from GTB3Window import rawMainWindow
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from GTBWindow import Ui_MainWindow
 
 
-class MainWindow(rawMainWindow, QMainWindow, QWidget):
-    signal_event_open_file = pyqtSignal()
-
+class Window(Ui_MainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super(Window, self).__init__()
 
-    def setupUi(self, window_object):
-        super(MainWindow, self).setupUi(window_object)
-
-        # Links go here
-        self.pushButton_open_file.clicked.connect(self.signal_emitter)
-
-    def signal_emitter(self):
-        sender = self.sender()
-        if sender == self.pushButton_open_file:
-            print(True)
-            self.signal_event_open_file.emit()
-
-    def set_window_file_path(self):
-        path = GTB3Applications.Application.getFilePath()
-        self.lineEdit_file_path.setText(path)
-
-
-if __name__ == '__main__':
-    import sys
-
-    app = QApplication(sys.argv)
-    ui = MainWindow()
-    window = QMainWindow()
-    ui.setupUi(window)
-    window.show()
-    sys.exit(app.exec_())
+    def setupUi(self):
+        super(Window, self).setupUi()
+        # Setup Connections
